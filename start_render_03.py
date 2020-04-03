511a6bcdd7b76b34a2970442c7967098c69a3e77
cigar666 <37494715+cigar666@users.noreply.github.com>
2020-04-03 14:09:28 +0800

    add more start_render.py

---
+++ start_render_03.py
@@ -0,0 +1,22 @@
+import os
+f = open('start_render_03.bat', 'w')
+
+path_list = ['',                                           # 0
+             'my_manim_projects\\',                        # 1
+             'my_manim_projects\\my_projects\\',           # 2
+             'my_manim_projects\\test\\',                  # 3
+             'manim_sandbox\\utils\\',                     # 4
+             'my_manim_projects\\my_utils\\',              # 5
+             ]
+
+py_file_name = path_list[2] + 'Pascal_triangle.py'
+classname = 'Picture'
+
+pl = '-pl'
+pm = '-pm'
+high = '--high_quality'
+
+str01 = 'python -m manim ' + py_file_name + ' ' + classname + ' ' + '-s'# pm # high # + ' -p'
+f.write(str01)
+f.close()
+os.system('start_render_03.bat')
