17463ee2d8da4ce862b6931cc92e3d6d55fb0c50
cigar666 <37494715+cigar666@users.noreply.github.com>
2020-05-30 12:33:59 +0800

    add some codes to render 4k video directly

--- start_render.py
+++ start_render.py
@@ -7,16 +7,22 @@ path_list = ['',                                           # 0
              'my_manim_projects\\test\\',                  # 3
              'manim_sandbox\\utils\\',                     # 4
              'my_manim_projects\\my_utils\\',              # 5
+             'my_manim_projects\\my_projects\\manim_code_by_manim\\',            # 6
+             'manim_sandbox\\videos\\HomeworkVol05\\',                           # 7
+             'others\\',                                   # 8
+             'my_manim_projects\\my_projects\\fractal\\',            # 9
+             'manim_sandbox\\utils\\mobjects\\',                     # 10
              ]
 
-py_file_name = path_list[5] + 'test_3D.py'
-classname = 'Play_rubic_order3'
+py_file_name = path_list[3] + 'Text_test.py'
+classname = 'Test_02'
 
 pl = '-pl'
 pm = '-pm'
 high = '--high_quality'
+uhd = '-uhd'
 
-str01 = 'python -m manim ' + py_file_name + ' ' + classname + ' ' + pm # high # + ' -p'
+str01 = 'python -m manim ' + py_file_name + ' ' + classname + ' ' + uhd # high + ' -s'# + ' -t'
 f.write(str01)
 f.close()
 os.system('start_render.bat')
